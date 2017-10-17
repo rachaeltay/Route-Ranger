83ed3c17eb74101f6a803bf49592dc6921bcccf2
soraares <soraares@gmail.com>
2017-10-17 20:30:20 +0800

    plot + date selection

---
+++ bus stop usage module/server.R
@@ -0,0 +1,97 @@
+library(shiny)
+library(mongolite)
+library(jsonlite)
+library(DT)
+library(ggplot2)
+
+queryList <- mongo(db = "local", collection = "queryList")
+
+server <- function(input, output) {
+  getStartDate <- function(date) {
+    if (input$timeFrame == "Monthly") {
+      paste0(substr(date,0,8), "01", 'T00:00:00Z')
+    } else {
+      paste0(date, 'T00:00:00Z')
+    }
+  }
+  
+  getEndDate <- function(date) {
+    if (input$timeFrame == "Monthly") {
+      paste0(substr(date,0,8), "31", 'T00:00:00Z')
+    } else {
+      paste0(date, 'T00:00:00Z')
+    }
+  }
+  
+  getBoarding <- eventReactive(input$genResult, {
+    if (input$timeFrame == "Hourly") {
+      query <- paste0('{"sourceBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate1), '"},"$lt":{"$date":"', getEndDate(input$startDate1+1),'"}}}')
+    } else if (input$timeFrame == "Monthly") {
+      query <- paste0('{"sourceBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate2), '"},"$lt":{"$date":"', getEndDate(input$endDate+1), '"}}}')
+    } else {
+      query <- paste0('{"sourceBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate2), '"},"$lt":{"$date":"', getEndDate(input$endDate+1), '"}}}')
+    }
+    queryList$find(query)
+  })
+  
+  getAlighting <- eventReactive(input$genResult, {
+    if (input$timeFrame == "Hourly") {
+      query <- paste0('{"destinationBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate1), '"},"$lt":{"$date":"', getEndDate(input$startDate1+1),'"}}}')
+    } else if (input$timeFrame == "Monthly") {
+      query <- paste0('{"destinationBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate2), '"},"$lt":{"$date":"', getEndDate(input$endDate+1), '"}}}')
+    } else {
+      query <- paste0('{"destinationBusStop":"', isolate(input$busStop), '","timestamp":{"$gte":{"$date":"', getStartDate(input$startDate2), '"},"$lt":{"$date":"', getEndDate(input$endDate+1), '"}}}')
+    }
+    queryList$find(query)
+  })
+  
+  output$boarding <- renderText({
+    print(nrow(getBoarding()))
+  })
+  
+  output$alighting <- renderText({
+    print(nrow(getAlighting()))
+  })
+  
+  output$plot <- renderPlot({
+    busStop <- isolate(input$busStop)
+    timeFrame <- isolate(input$timeFrame)
+    boarding <- getBoarding()
+    alighting <- getAlighting()
+    
+    if (timeFrame == "Hourly") {
+      boarding <- table(cut(boarding$timestamp, breaks="hour"))
+      alighting <- table(cut(alighting$timestamp, breaks="hour"))
+    } else if (timeFrame == "Daily") {
+      boarding <- table(cut(boarding$timestamp, breaks="day"))
+      alighting <- table(cut(alighting$timestamp, breaks="day"))
+    } else if (timeFrame == "Weekly") {
+      boarding <- table(cut(boarding$timestamp, breaks="week"))
+      alighting <- table(cut(alighting$timestamp, breaks="week"))
+    } else if (timeFrame == "Monthly") {
+      boarding <- table(cut(boarding$timestamp, breaks="month"))
+      alighting <- table(cut(alighting$timestamp, breaks="month"))
+    }
+    
+    boarding <- data.frame(boarding)
+    boarding$Group <- paste0("Boarding from ", busStop)
+    colnames(boarding) <- c("timeFrame", "Count", "Group")
+    alighting <- data.frame(alighting)
+    alighting$Group <- paste0("Alighting from ", busStop)
+    colnames(alighting) <- c("timeFrame", "Count", "Group")
+    combined <- rbind(alighting, boarding)
+    
+    plot <- ggplot(combined, aes(x=timeFrame, y=Count, group=Group, color=Group)) + geom_line()
+    plot <- plot + labs(title=paste0('Number of riders boarding at ', busStop), y="# of riders", x="")
+    plot <- plot + theme(panel.background=element_rect(fill="black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+    plot
+  })
+  
+  output$testText <- renderText({
+    
+  })
+  
+  output$testTable <- DT::renderDataTable({
+    getBoarding()
+  })
+}
---
+++ bus stop usage module/ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+stops <- c("KR_terminal", "UHC", "Central_Library", "Kent_RIdge_MRT", "COM2", "Museum", "BIZ2", "Opp_HSSML", "LT13", "Ventus", "PGP_Hse_12", "Computer_Centre", "PGP_Hse_14_15", "PGP_Hse_7", "YIH", "Opp_YIH", "UHall", "Opp_UHall", "PGP", "Raffles_Hall", "EA", "PGPR", "LT29", "S17", "Opp_Kent_Ridge_MRT")
+timeIntervals <- c("Monthly", "Weekly", "Daily", "Hourly")
+
+ui <- fluidPage(
+  titlePanel(div(h1("The Route Ranger"), style="color:red")),
+  sidebarLayout(
+    sidebarPanel(selectInput("busStop", "Select Bus Stop", sort(stops), selected="COM2"),
+                 selectInput("timeFrame", "Select Time Frame", timeIntervals, selected="Daily"),
+                 conditionalPanel("input.timeFrame == 'Hourly'", dateInput("startDate1", "Choose a Day")),
+                 conditionalPanel("input.timeFrame != 'Hourly'", dateInput("startDate2", "Starting Date"), dateInput("endDate", "Ending Date")),
+                 actionButton("genResult" , "Show Stop Usage!")
+                 ),
+    mainPanel(div(tags$label("Number of people boarding"), textOutput("boarding"),
+              tags$label("Number of people aighting"), textOutput("alighting"), style="color:teal"), br(),
+              div(plotOutput("plot"), textOutput("testText"), dataTableOutput("testTable"))
+              )
+              )
+  )
